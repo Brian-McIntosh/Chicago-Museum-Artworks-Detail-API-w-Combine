e8298443040892eaff44d5be9d53fb2a93959af1
Brian McIntosh <brian@mixinteractive.com>
2023-04-28 10:22:47 -0400

    Initial Commit

---
+++ Combine-Artworks-API/Combine_Artworks_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_Artworks_APIApp.swift
+//  Combine-Artworks-API
+//
+//  Created by Brian McIntosh on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Combine_Artworks_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
